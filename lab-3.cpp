cdb466ce06dfb0ef2cf890cfc24891e35dd156ab
AnhMinh6 <145763860+AnhMinh6@users.noreply.github.com>
2023-10-06 11:30:08 +0300

    Add files via upload

---
+++ lab-3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+long int min_difference = 100000000+1; 
+int n;
+int a[] = {};
+int f(long int petya,long int masha, int i) {
+ if (i == n) {
+  if (abs(petya - masha) < min_difference) {
+   min_difference = abs(petya - masha);
+  }
+ }
+ else {
+  f(petya + a[i], masha, i + 1);
+  f(petya, masha + a[i], i + 1);
+ }
+ return 0;
+}
+
+int main() {
+ cin >> n;
+ for (int i = 0; i < n; i++) {
+  cin >> a[i];
+ }
+ f(0, 0, 0);
+ cout << min_difference;
+ return 0;
+}
